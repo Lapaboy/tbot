0e5a814a1cb489185bdca5a2a1d43d0ceb2258f0
artem.lapko <artyom.lapko@ostrovok.ru>
2018-04-21 21:01:00 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const TelegramBot = require('node-telegram-bot-api');
+const exec = require('child_process').exec;
+
+const ERRORS = {
+    NOT_PRIVATE: 'Chat should be private!'
+}
+const TOKEN = '577021868:AAFW3ltJM4NAs75LYJPta19Ac7bFKX1JBLU';
+const bot = new TelegramBot(TOKEN, {polling: true});
+
+
+bot.onText(/(.*)/, function (msg, match) {
+    const userId = msg.from.id;
+    if (msg.chat.type === 'private') {
+        //log request from chat
+        bot.sendMessage(userId, ERRORS.NOT_PRIVATE);
+        return;
+    }
+
+    //escape here
+    exec(`id -u ${msg.from.username}`, (err, stdout) => {
+        if (typeof parseInt(stdout) !== 'number') {
+            createUser(msg.from.username, userId);
+        } else {
+            //log existed user reqesting url
+            showUrl(msg.from.username, userId);
+        }
+    });
+});
+
+function createUser(username, userId) {
+    //escape here
+    exec(`echo ${username} | passwd ${username} --stdin`, (err, stdout) => {
+        //log usercreated
+        console.log('user created?\n', stdout);
+        showUrl(username, userId);
+    });
+};
+
+function showUrl(username, usrId) {
+    const url = getFormattedUrl(username);
+    bot.sendMessage(userId, url);
+}
